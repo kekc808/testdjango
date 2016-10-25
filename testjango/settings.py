e663390cd75789017a6f521898b922b525bf97f1
UniqWeb <reatrof@gmail.com>
2016-10-25 21:47:45 +0300

    new personal app and contact page

--- testjango/settings.py
+++ testjango/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls',
+    'personal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
